0789f5a96040a7398fa9d79de0f8439d38788931
Duncan Temple Lang <duncan@r-project.org>
2026-01-11 19:55:43 -0800

    numerous improvements. Compute sponsor and sponsor's department as part of result. Return data.frames. Combine for each page. Combine across pages.  Also compute which pages are abstract pages.

--- funs2.R
+++ funs2.R
@@ -1,12 +1,77 @@
+if(FALSE) {
+
+    xml = "25_URC_Abstract_Book_V2-1.xml"
+    doc = readPDFXML(xml, encoding = "UTF8")
+    pgs = doc[49:313]
+    a = lapply(pgs, \(p) try(doPage(p)))
+    e = sapply(a, inherits, 'try-error')
+    b = do.call(rbind, a)
+    rownames(b) = NULL
+    b$numWords = sapply(strsplit(b$text, " +"), length)
+    b$dept = cleanDeptName(b$dept)
+    
+    stopifnot(length(grep("Sponsor", b$sponsor)) == 0)
+}
+
+if(FALSE) {
+
+    # no longer needed as we are computing sponsor and dept in doPage()
+    
+    sp = getSponsors(doc)
+    sp = sp[sp$page >= 49,]
+    # 2 currently not the same due to different fonts for accents.
+#    stopifnot(b$sponsor == sp$name)
+    b$dept = sp$dept
+}
+
+if(FALSE) {
+
+    # Finding department names that are similar.
+
+    d = sort(unique(b$dept))
+    dt = adist(d)
+    dimnames(dt) = list(d, d)
+    tmp = structure(sapply(1:nrow(dt), \(i) { x = dt[i, -i]; names(x)[ x < 5]}), names = d)
+    tmp[sapply(tmp, length) > 0]
+
+    # Anr Plant Sciences and Plant Sciences
+    # Ag Molecular & Cellular Bio
+    # Int Med Cardiology (sac) and Int Med Cardiology (davis)
+}
+
+
 FontInfo = list(title = 36, studentName = c(62, 76), abstract = c(65, 74, 78), sponsor = c(64, 73))
+
+getAbstracts =
+function(doc, pages = doc[abstractPages(doc)], fonts = FontInfo, ...)    
+{
+    if(is.character(doc))
+        doc = readPDFXML(doc, ...)
+
+    a = lapply(pages, \(p) try(doPage(p)))
+    e = sapply(a, inherits, 'try-error')
+    if(any(e))
+        warning("errors reading ", sum(e), " abstracts")
+
+    b = do.call(rbind, a)
+    rownames(b) = NULL
+    b$numWords = sapply(strsplit(b$text, " +"), length)
+    b$dept = cleanDeptName(b$dept)
+    b
+}
+
 
 doPage =
 function(p, bb = getBBox2(p, TRUE, attrs = c("left", "top", "font")),
          fonts = FontInfo,
          lines = getBBox(p, TRUE))
 {
-    if(nrow(lines))
-        bb = bb[ bb$top + bb$height < min(lines$y0),]
+    if(nrow(lines)) {
+        lines = lines[(lines$x1 - lines$x0) > 30,]
+        bb = bb[ (bb$top + bb$height) < min(lines$y0),]
+    }
+   
+
     v = getProjects(bb, midPoint(p), fonts)
     v$pageNum = as.integer(xmlGetAttr(p, "number"))
     v
@@ -23,10 +88,13 @@ getProjects =
 function(bb, mid, fonts = FontInfo)
 {
     cols = split(bb, bb$left > mid) # max(bb$left + bb$width)/2)
-    structure(lapply(cols, splitCol, fonts), names = paste0("column", 1:2))
+
     v0 = lapply(cols, splitCol, fonts)
     v = do.call(rbind, v0)
     v$column = rep(1:2, sapply(v0, nrow))
+    tmp = v$sponsor
+    v$sponsor = cleanSponsorName(tmp)
+    v$sponsorTitle = getSponsorTitle(tmp)
     v
 }
 
@@ -35,13 +103,10 @@ function(bb, fonts = FontInfo)
 {
     bb = bb[order(bb$top),]
     d = diff(bb$top)
-#    browser()
-#    stopifnot(all(d >= 0))
+    #    stopifnot(all(d >= 0))
+
     i = which.max(d) 
     as = split(bb, 1:nrow(bb) > i)
-#    w = bb$font %in% fonts$title
-#    r = rle(w)
-#    bb[w,]
 
     v = lapply(as, mkAbstract, fonts)
     v = do.call(rbind, v)
@@ -60,23 +125,69 @@ function(bb, fonts = FontInfo)
     ti = bb[bb$top < min(bb$top[w]), ]
     txt = bb[bb$top >= min(bb$top[ bb$font %in% fonts$abstract]), ]
 
-    as.data.frame(lapply(list(title = ti, text = txt, student = bb[w,]),
+    ans = as.data.frame(lapply(list(title = ti, text = txt, student = bb[w,]),
            #             sponsor = sponsor, department =
-                         combineText))
-#   list(title = combineText(ti),
-#        text = combineText(txt),
-#        student = combineText(bb[w,]),
-#        sponsor = combineText(sponsor)
-#       )
+                               combineText))
+    ans$sponsor = getSponsorText(bb)
+    ans$dept = getSponsorDept(bb)
+    ans
 }
+
+
+getSponsorText =
+function(bb, threshold = 2)    
+{
+    i = grep("Sponsor:", bb$text)
+    bb2 = bb[ abs(bb$top - bb$top[i]) < threshold, ]
+    #    ReadPDF:::combineLines(bb2)
+    combineText(bb2$text[order(bb2$left)])
+}
+
+getSponsorDept =
+function(bb, threshold = 2)    
+{
+    i = grep("Sponsor:", bb$text)
+    bb2 = bb[ bb$top > bb$top[i], ]
+    bb2 = bb2[ bb2$top == min(bb2$top),]
+    combineText(bb2)
+}
+
 
 combineText =
 function(bb)
 {
-    normalizeSpace( paste(bb$text, collapse = " ") )
+    if(is.data.frame(bb))
+        bb = bb$text
+    normalizeSpace( paste(bb, collapse = "") )
 }
 
 
 normalizeSpace =
 function(x)    
     trimws(gsub("[[:space:]]+", " ", x))
+
+
+abstractPages =
+function(doc)
+{
+    if(is.character(doc))
+        doc = readPDFXML(doc, ...)
+
+    as.integer(getNodeSet(doc, "//page[contains(., 'Sponsor: ')]/@number"))
+}
+
+getFontInfo =
+function(doc, pages = doc[abstractPages(doc)])
+{
+    if(is.character(doc))
+        doc = readPDFXML(doc, ...)
+
+    bbs = lapply(pages, getBBox2, TRUE, attrs = c("left", "top", "font"))
+    # For each page, 
+    # look for title at the top of the two columns
+    # Sponsor: to get the font
+    # abstract - last several lines of each column
+    # department -
+    # student name - line above Sponsor: 
+    sapply(
+}
