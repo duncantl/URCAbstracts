1d1ee3b15afd21dd85ee33c17e7160775e64f91b
Duncan Temple Lang <duncan@r-project.org>
2026-01-11 19:54:00 -0800

    split code to process sponsor name and title. Also department name to avoid unnecessary duplicates with slightly different variations

--- funs.R
+++ funs.R
@@ -1,6 +1,10 @@
 library(XML)
 library(ReadPDF)
-       
+
+
+# Problem with 1st abstract on page 145 of 2025.
+# Accent on character in sponsor's name means don't get the entire name.
+
 getSponsors = 
 function(doc, addPageIdentifier = TRUE)
 {
@@ -10,15 +14,35 @@ function(doc, addPageIdentifier = TRUE)
     s = getNodeSet(doc, "//text[starts-with(normalize-space(.), 'Sponsor:')]")
     
     s2 = sapply(s, xmlValue)
-    ans = gsub("Sponsor: ?", "", s2)
-    ans = data.frame(name = gsub(",.*", "", ans), title = gsub("^[^,]+,", "", s2)) # , txt = s2)
+    ans = data.frame(name = cleanSponsorName(s2), title = getSponsorTitle(s2)) # , txt = s2)
     ans$dept = gsub("&amp;", "&", sapply(s, getDept))
     if(addPageIdentifier)
         ans$page = sapply(s, pageOf)
 
+    ans$dept = cleanDeptName(ans$dept)
+    
     ans
 }
 
+cleanDeptName =
+function(dept)
+{
+    dept = gsub("&amp;", "&", dept)
+    dept = gsub("^((Anr )|Ag (?!&))", "", dept, perl = TRUE)
+    dept = gsub("\\(.*\\)$", "", dept)
+}
+
+
+cleanSponsorName =
+function(x)    
+{
+    ans = gsub("Sponsor: ?", "", x)
+    gsub(",.*", "", ans) 
+}
+
+getSponsorTitle =
+function(x)
+    trimws(gsub("^[^,]+,", "", x))
 
 getDept =
 function(x)
