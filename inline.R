49cba1eb552dbae46685d7bbf0808aac6018d163
Duncan Temple Lang <duncan@r-project.org>
2026-01-12 18:00:33 -0800

    general function to inline JS and JSON content into an HTML to make standalone

---
+++ inline.R
@@ -0,0 +1,27 @@
+inline =
+function(doc, out = character())
+{
+    if(is.character(doc))
+        doc = htmlParse(doc, encoding = "UTF8")
+
+    sc = getNodeSet(doc, "//script[@src]")
+    ff = sapply(sc, xmlGetAttr, "src")
+    content = sapply(ff, function(x) paste(readLines(x, warn = FALSE), collapse = "\n"))
+    
+    mapply(inlineScript, sc, content)
+
+    if(length(out) && !is.na(out))
+        saveXML(doc, out)
+    else
+        doc
+}
+
+inlineScript =
+function(node, content)    
+{
+    newXMLTextNode(content, parent = node, cdata = TRUE)
+    a = xmlAttrs(node)
+    a = a[names(a) != "src"]
+    xmlAttrs(node, append = FALSE) = a
+    node
+}
